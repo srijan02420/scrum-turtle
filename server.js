805936c37b061b142920df30030ee5d716f63816
Bharat Vishwakarma <bharat@dream11.com>
2018-06-14 17:10:25 +0530

    diet

--- server.js
+++ server.js
@@ -3,8 +3,14 @@ const app = express()
 
 app.use(express.static(__dirname + '/public'))
 
+
+
 app.get('/', function(req, res) {
     res.sendfile(__dirname + '/public/index.html')
+})
+
+app.get('/diet', function(req, res) {
+    res.sendfile(__dirname + '/public/diet.html')
 })
 
 app.listen(3000, () => {
